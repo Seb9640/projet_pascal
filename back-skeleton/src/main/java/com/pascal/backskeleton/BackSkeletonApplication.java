a53c594b9dc0e7d757f822ec7a2e01e30aab35a6
Espero AKPOLI <mongodbn@gmail.com>
2024-05-09 18:03:31 +0200

    update

--- back-skeleton/src/main/java/com/pascal/backskeleton/BackSkeletonApplication.java
+++ back-skeleton/src/main/java/com/pascal/backskeleton/BackSkeletonApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@ComponentScan("com.pascal.backskeleton.webconfig")
 @ComponentScan("com.pascal.backskeleton.controllers")
 @ComponentScan("com.pascal.backskeleton.services")
-@ComponentScan("com.pascal.backskeleton.webconfig")
 @ComponentScan("com.pascal.backskeleton.exception")
 public class BackSkeletonApplication {
     public static void main(String[] args) {
