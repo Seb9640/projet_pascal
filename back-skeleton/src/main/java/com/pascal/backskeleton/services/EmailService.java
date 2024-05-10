0bedaad3aa54d6d4452aac947c6bd803b5294afa
Espero AKPOLI <mongodbn@gmail.com>
2024-05-10 09:17:56 +0200

    update

--- back-skeleton/src/main/java/com/pascal/backskeleton/services/EmailService.java
+++ back-skeleton/src/main/java/com/pascal/backskeleton/services/EmailService.java
@@ -3,6 +3,10 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 
 import com.pascal.backskeleton.models.User;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
 import org.springframework.stereotype.Service;
 
 
